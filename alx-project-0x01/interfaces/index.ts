2d8ecb5b45e027749eea10b4ef59300086cf7876
fatimah oladipo <fatimaholadipo4@gmail.com>
2025-09-20 22:11:42 +0100

    task 4 complete

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -29,21 +29,14 @@ export interface UserProps {
     };
 }
 
-export interface Address {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-    geo: Geo;
+export interface PostData {
+  userId: number;
+  id?: number;
+  title: string;
+  body: string;
 }
 
-export interface Geo {
-    lat: string;
-    lng: string;
-}
-
-export interface Company {
-    name: string;
-    catchPhrase: string;
-    bs: string;
+export interface PostModalProps {
+  onClose: () => void;
+  onSubmit: (post: PostData) => void;
 }
--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,6 +1,8 @@
 import PostCard from "@/components/common/PostCard";
+import PostModal from "@/components/common/PostModal";
 import Header from "@/components/layout/Header";
-import { PostProps } from "@/interfaces";
+import { PostProps, PostData } from "@/interfaces";
+import { useState } from "react";
 
 
 
@@ -9,22 +11,36 @@ interface PostsPageProps {
 }
 
 const Posts: React.FC<PostsPageProps> = ({ posts }) => {
-  console.log(posts)
+    const [isModalOpen, setModalOpen] = useState(false);
+    const [post, setPost] = useState<PostData | null>(null);
+
+    const handelAddPost = (newPost: PostData) => {
+        setPost({...newPost, id: posts.length + 1})
+    };
+
   return (
     <div className="flex flex-col h-screen">
       <Header />
       <main className="p-4">
         <div className="flex justify-between">
         <h1 className=" text-2xl font-semibold">Post Content</h1>
-        <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
+        <button onClick={()=>setModalOpen(true)}
+        className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
         </div>
         <div className="grid grid-cols-3 gap-2 ">
+          {post && <PostCard title={post.title} body={post.body} userId={post.userId} id={post.id? post.id: 1} />}
+
           {
             posts?.map(({ title, body, userId, id }: PostProps, key: number) => (
               <PostCard title={title} body={body} userId={userId} id={id} key={key} />
             ))
           }
         </div>
+
+        {isModalOpen && (
+            <PostModal onClose={()=>setModalOpen(false)} onSubmit={handelAddPost} />
+        )
+        }
       </main>
     </div>
   )
